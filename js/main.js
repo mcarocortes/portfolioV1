0759851c7e0aa834d4f7efda4a312d2e50bc6907
Macarena Caro <m.caro.cortes2@gmail.com>
2025-06-26 21:02:11 +0200

    spline

--- js/main.js
+++ js/main.js
@@ -8,5 +8,18 @@
 })(window, document);
 
 
+import { Application } from 'https://unpkg.com/@splinetool/runtime@1.0.31/build/runtime.js';
 
+const canvas = document.getElementById('canvas3d');
+const loader = document.getElementById('loader');
+const app = new Application(canvas);
+
+const splineURL = 'scene.splinecode';
+
+app.load(splineURL).then(() => {
+  loader.style.display = 'none';
+}).catch(err => {
+  console.error('Error al cargar la escena:', err);
+  loader.innerHTML = '<p>Error al cargar la escena</p>';
+});
 
