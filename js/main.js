e1bfae98532e223bc41647fcd3cff6ef5ed0f272
Macarena Caro <m.caro.cortes2@gmail.com>
2025-06-26 21:09:00 +0200

    mimi

--- js/main.js
+++ js/main.js
@@ -10,14 +10,12 @@
 
 import { Application } from 'https://unpkg.com/@splinetool/runtime@1.0.31/build/runtime.js';
 
-const canvas = document.getElementById('canvas3d');
-const loader = document.getElementById('loader');
+const canvas = document.getElementById('canvas');
 const app = new Application(canvas);
 
-const splineURL = 'sceneMimi.splinecode';
+const splineURL = 'scene.splinecode';
 
 app.load(splineURL).then(() => {
-  loader.style.display = 'none';
 }).catch(err => {
   console.error('Error al cargar la escena:', err);
   loader.innerHTML = '<p>Error al cargar la escena</p>';
